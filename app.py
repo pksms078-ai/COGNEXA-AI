9321d45a8b742a44eabebb4737e8cc95b7a68a2e
Prabin Kumar <pksms078@gmail.com>
2026-06-01 17:40:12 +0530

    Initialize Flask app with home route

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
+Python
+from flask import Flask
 
+app = Flask(__name__)
+
+@app.route("/")
+
+def home():
+
+    return {
+        "platform":"COGNEXA",
+        "status":"running"
+    }
+
+if __name__ == "__main__":
+    app.run()
