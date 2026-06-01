c8e48da10d6d6baaf525f000b130e16dadd486d8
Prabin Kumar <pksms078@gmail.com>
2026-06-01 18:07:31 +0530

    Add initial API test for status check

--- test_api.py
+++ test_api.py
@@ -1,1 +1,15 @@
+Python
+import unittest
 
+class APITest(unittest.TestCase):
+
+    def test_status(self):
+
+        self.assertEqual(
+            200,
+            200
+        )
+
+if __name__ == "__main__":
+
+    unittest.main()
